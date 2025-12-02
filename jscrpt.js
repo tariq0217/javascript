449f316de4fe18ee8c2cc4a51df8c6977b826fc1
tariq0217 <amohammedtariq02@gmail.com>
2025-12-02 10:33:56 +0530

    Add files via upload

---
+++ jscrpt.js
@@ -0,0 +1,213 @@
+// let uName = "Ecma"
+// console.log( uName );
+
+
+// let lName= "javascript"
+// let LName= "dynmiclly typed"
+// console.log( lName,LName )
+
+
+// let $lName= "single page appliction"  
+// let _lName= "SPA"  
+
+// //let 0lName = "tariq" //numeric value is not allowd in begining
+
+// let Name1 = "aslam"
+
+
+// console.log( $lName,_lName );
+
+// console.log(Name1);
+
+// //imprtnt 3 cases
+
+// //camel case
+// //pascal case
+// //under case   //ex:newemployeeid
+
+// let newEmployeeId =10; //camel case
+
+// let NewEmployeeId =10; //pascal case
+
+// let New_Employee_Id =10; //under case
+// let new_employee_id =10; //under case
+
+// //VAR declaration & intialization 
+
+// var age; //declration
+
+// age = 20; //intialization
+
+// console.log( age );
+
+// //re-declaration
+
+// var age = 30; //re-declaration
+// console.log( age ); 
+
+// //re-initialization or assign
+// age="Tariq" //re-initialization or assign
+// console.log( age );
+
+// //LET declaration & intialization 
+
+// let newage; //declration
+
+// newage = 20; //intialization
+
+// console.log( newage );
+
+// //re-declaration
+
+// // let newage = 30; // cannot re-declaration in let
+// // console.log( newage ); 
+
+// //re-initialization or assign
+// newage="Tariq" //re-initialization or assign
+// console.log( newage );
+
+// //CONST declaration & intialization this is fixed
+
+// const  empage = 20; //intialization
+
+// console.log( empage );
+
+//re-declaration
+
+// const empage = 30; // cannont re-declaration in const
+// console.log( empage ); 
+
+//re-initialization or assign
+// empage="Tariq" // cannot re-initialization or assign in const
+// console.log( empage );
+
+
+//PRINTING STATEMENT
+
+// alert( age )
+// document.write(age)
+// confirm(age)
+
+
+// let userage = prompt("enter ur age")
+// console.log(userage);
+// let userage = 10;
+// console.error(userage); //op in red color
+// console.warn(userage); // op in yellow color 
+
+// console.clear() // comnsole cleared 
+
+
+//PRIMITIVE DATA TYPE
+
+// 1.NUMBER
+
+var num = 120;
+var num = 1.25;
+
+//2.string
+
+let userName = "javascript is a  scripting language"
+userName = 'javascript is a single thread language'
+userName = `ecma script`
+
+//3.boolean
+
+let condition = true;
+condition = false;
+
+//4. undefined
+
+let noValue ;
+
+//5.null
+
+let emptyValue = null;
+console.log(num , userName,condition, noValue);
+
+//single line comment 
+/* MULTI line comment
+     multi line comment
+     wkkvvbjbhjabsvjna
+     akjvhaejk
+     ajvabn
+*/
+
+
+//NON PRIMITIVE DATATYPE
+
+//1.Array []
+
+let multileUsers = ["reactjs" , "jvascript", "facenbook" , "instagram"]
+//index              0             1            2             3      
+console.log( multileUsers);
+
+console.log(multileUsers[0],multileUsers[2] , multileUsers[3]);
+
+console.log(multileUsers.length);
+console.log(multileUsers.length-1);
+console.log(multileUsers[multileUsers.length-1]);
+
+
+//OBJECT {} -key value pair
+
+let vehicle = {
+    vehicleType : "Four Wheeler" ,
+    brand : "tata",
+    price : 100000,
+    fuelType : "petrol"
+
+}
+
+console.log(vehicle);
+
+console.log(vehicle.price);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
