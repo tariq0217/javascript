fd2f6447796e74e04f7771894342b92c492cf786
tariq0217 <amohammedtariq02@gmail.com>
2025-12-02 11:26:57 +0530

    ep:8 operators learn with examples

---
+++ operators.js
@@ -0,0 +1,40 @@
+//arithmetic operator 
+
+// console.log(10 + 20);
+
+// console.log(30 - 20);
+
+// console.log(20 * 2);
+
+// console.log(20 / 4); //quetient
+
+// console.log(20 % 3); // modulus --> reminder => 2
+
+// console.log(2 ** 4); //exponent 
+
+//increment - decrement 
+// let num = 10;
+
+// num = num+1
+
+// console.log(num);
+
+//increment => (++)
+  
+    let num = 20;
+     num++; //--> post increment
+     ++num; // --> preincrement
+    console.log(num);
+       
+ 
+//decrement => (--)
+
+  let num1 = 20;
+     num1--; //--> post decrement
+     --num1; // --> predecrement
+    console.log(num1);
+       
+
+
+
+
