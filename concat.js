200b6904acd56fa40689a5ea943eb6a91678f928
tariq0217 <amohammedtariq02@gmail.com>
2025-12-09 19:14:26 +0530

    ep-12: learn concat and template string / literal

---
+++ concat.js
@@ -0,0 +1,23 @@
+//Concat
+
+let str = "Hello "
+let str1 = "John"
+
+let output = str + str1
+console.log(output);
+
+console.log(output + " Welcome to our website");
+
+//Templte string or Template Literal (``)
+
+let newStr = `Jvascript`;
+
+let newStr1 = `${newStr} is a scripting lnguage`
+let num = 5;
+let first =1 ;
+
+console.log(num + "*" + first + "=" + num*first);
+console.log(`${num}*${first} = ${num*first}`);
+
+
+
