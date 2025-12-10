5084f38864bf14094f25e04da7376770a2313ab7
tariq0217 <amohammedtariq02@gmail.com>
2025-12-10 21:44:30 +0530

    ep-13:learn control statement

---
+++ controlStatement.js
@@ -0,0 +1,50 @@
+//IF Statement
+
+/*
+if( cond){
+    statement
+}
+*/
+
+// let uAge = 25;
+// if (uAge >= 18){
+//     console.log("He is eligible to Vote");
+// }
+
+//IF-ELSE Statement
+
+/*
+ if (){
+ statement
+ }
+
+ else{
+    statement
+    }
+
+*/
+
+
+let uAge = 17;
+
+if (uAge >= 18)
+{
+    console.log("He is Eligible to Vote");
+    
+}
+else{
+    console.log("He is not eligible to");
+    
+}
+
+let pswd_input = true;
+
+if(pswd_input){
+    console.log("Welcome to Website");
+    
+}
+else{
+    console.log("Incorrect Password");
+    
+} 
+//
