4f280f1bfb9d428ead22c7c869f6e8b2c09050cd
tariq0217 <amohammedtariq02@gmail.com>
2025-12-21 12:46:17 +0530

    ep-23 : Learn return type and non return type

--- function.js
+++ function.js
@@ -1,19 +1,55 @@
-let person1 = {
-    name : "tariq"
-}
+// // // let person1 = {
+// // //     name : "tariq"
+// // // }
 
-let empId ;
-let newId =  empId || 3214;
-console.log(newId);
+// // // let empId ;
+// // // let newId =  empId || 3214;
+// // // console.log(newId);
 
-function printUserName(uName = "tariq", uAge = 18){
+// // // function printUserName(uName = "tariq", uAge = 18){
 
-    // if (uAge <= 20)
-    console.log(`hi ${uName} , your age is ${uAge}` );
+// // //     // if (uAge <= 20)
+// // //     console.log(`hi ${uName} , your age is ${uAge}` );
+    
+// // // }
+
+// // // printUserName("tariq" , 19)
+// // // printUserName("aslm" , 13)
+// // // printUserName("harish" , 20)
+// // // printUserName()
+
+// // function rectArea(l , b) {
+// //     // console.log("Finding Are");
+
+// //     let cond = false
+
+// //     if (cond) {
+// //     return l *b
+        
+// //     } else{
+// //     return null 
+// //     }
+    
+// // }
+// // let newVal = rectArea(10, 40)
+// // console.log(rectArea(10 , 50) , newVal);
+
+
+// function cubic(num){
+//     // console.log(num**3);
+    
+//     return ( num**3 )
+// }
+
+// let newVal = cubic(9);
+// console.log(newVal);
+
+
+function name1(){
+    console.log("Non return Type");
+    return 12
     
 }
 
-printUserName("tariq" , 19)
-printUserName("aslm" , 13)
-printUserName("harish" , 20)
-printUserName()
+let retVal = name1();
+console.log(retVal);
