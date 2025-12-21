3930a1607e5e21018b90482c75be8fb93b3f8680
tariq0217 <amohammedtariq02@gmail.com>
2025-12-21 20:35:38 +0530

    Ep-25 : Learn Funtion Types , Named , Ananomous, Arrow - Arrow easy methods

---
+++ functionTypes.js
@@ -0,0 +1,40 @@
+// //Named Function
+
+// function add(){
+//     console.log("Named Function");
+    
+
+// }
+// add()
+
+// //ANONOMOUS FUNCTION
+
+// let ananFun = function(){
+//     console.log("Ananomous Function");
+    
+// }
+
+// ananFun();// call var name for funcname
+
+// //ARROW FUNCTIOM
+
+// let funArrow = () => {
+//     console.log("Arrow function");
+    
+// }
+
+// funArrow();
+
+let newFun = name1=> console.log(name1);
+
+newFun("Tariq")
+
+
+let firstName = "Harish";
+
+let  returnFun = name2 => name2 
+
+let Val = returnFun("Abisheik");
+
+console.log(Val);
+
