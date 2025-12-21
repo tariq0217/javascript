37e7893faeac67f7f7a63c1f0889c1f97c7ab6fa
tariq0217 <amohammedtariq02@gmail.com>
2025-12-21 11:47:18 +0530

    Ep-21: Learn FunctionDeclaration , Calling

---
+++ function.js
@@ -0,0 +1,15 @@
+let person1 = {
+    name : "tariq"
+}
+
+function printUserName(uName , uAge){
+
+    if (uAge <= 20)
+    console.log(`hi ${uName} , your age is ${uAge}` );
+    
+}
+
+printUserName("tariq" , 19)
+printUserName("aslm" , 13)
+printUserName("harish" , 20)
+printUserName("abisheik" , 21)
