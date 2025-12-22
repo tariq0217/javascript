e5d5cdbebdf171f2624888fe3e72527e73aef425
tariq0217 <amohammedtariq02@gmail.com>
2025-12-22 19:11:32 +0530

    ep-26: Learn hoisting and temporal dead zone

--- functionTypes.js
+++ functionTypes.js
@@ -1,40 +1,72 @@
-// //Named Function
+// // //Named Function
 
-// function add(){
-//     console.log("Named Function");
+// // function add(){
+// //     console.log("Named Function");
     
 
-// }
-// add()
+// // }
+// // add()
 
-// //ANONOMOUS FUNCTION
+// // //ANONOMOUS FUNCTION
 
-// let ananFun = function(){
-//     console.log("Ananomous Function");
+// // let ananFun = function(){
+// //     console.log("Ananomous Function");
     
-// }
+// // }
 
-// ananFun();// call var name for funcname
+// // ananFun();// call var name for funcname
 
-// //ARROW FUNCTIOM
+// // //ARROW FUNCTIOM
 
-// let funArrow = () => {
-//     console.log("Arrow function");
+// // let funArrow = () => {
+// //     console.log("Arrow function");
     
-// }
+// // }
 
-// funArrow();
+// // funArrow();
 
-let newFun = name1=> console.log(name1);
+// let newFun = name1=> console.log(name1);
 
-newFun("Tariq")
+// newFun("Tariq")
 
 
-let firstName = "Harish";
+// let firstName = "Harish";
 
-let  returnFun = name2 => name2 
+// let  returnFun = name2 => name2 
 
-let Val = returnFun("Abisheik");
+// let Val = returnFun("Abisheik");
 
-console.log(Val);
+// console.log(Val);
 
+
+
+//HIGHER ORDER / CALL BACK 
+
+/*function function1() {
+    console.log("I am HO");
+    
+}
+
+function function2() {
+    console.log("I am call back");
+    
+}
+function1(function2())
+*/
+
+function add(callback ,a,b) {
+    callback(100,200)
+
+    console.log(a+b);
+    
+    
+}
+
+function sub(num1 , num2) {
+    
+    console.log(num1+num2);
+
+    
+}
+
+add(sub , 10,20)
