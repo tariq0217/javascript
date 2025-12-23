ecefc7d4f92c101748ffb224c9c73090885e0948
tariq0217 <amohammedtariq02@gmail.com>
2025-12-23 19:19:43 +0530

    Ep-30: Learn Arrays , arrays acces, dense and sparse array

---
+++ arrays.js
@@ -0,0 +1,21 @@
+//DS
+
+let flavours = ["vannila" , "butterscotch" , "lavender" , "chocolate"]
+// console.log(flavours);
+
+// console.log(flavours[0]);
+// console.log(flavours[1]);
+// console.log(flavours[2]);
+console.log(flavours[flavours.length - 1]);
+
+
+//DENSE 
+let denseArray= [1,2,3,4,5]
+
+// formila = baseAddress + (index*size)
+
+//Sparse Array
+
+let sparseArray = [10,20,,40,,60]
+
+console.log(sparseArray);
