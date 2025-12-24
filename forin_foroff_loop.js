ec00fc1f921c8e0ed3a40ee33c6b862a1e4a58e3
tariq0217 <amohammedtariq02@gmail.com>
2025-12-24 18:43:24 +0530

    ep-32:Learn iterative in for loop - array&string

---
+++ forin_foroff_loop.js
@@ -0,0 +1,17 @@
+//Array
+
+let arr = [10,20,30,40]
+
+
+for(let i = 0; i<arr.length ; i++){
+console.log(arr[i]);
+}
+
+//String
+
+let str = "javascript";
+
+for (let i = 0; i<str.length ; i++){
+    console.log(str[i]);
+    
+}
