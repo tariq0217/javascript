4efe4b04025a787509acc77f15817d468754fc9a
tariq0217 <amohammedtariq02@gmail.com>
2025-12-25 11:21:05 +0530

    Ep-37:Learn add-remove-replace/ push,pop,shift,unshift,splice

---
+++ add_remove_replce.js
@@ -0,0 +1,32 @@
+let arr = [10,20,30,40]
+
+//Pop - remove the last element 
+
+// arr.pop()
+
+//push - add the elements in the last
+
+// arr.push(100,200)
+
+// shift - removes the first element 
+
+// arr.shift()
+
+//unshift - adds the elements in the first
+
+// arr.unshift(2,3,4,5)
+
+//SPLICE - it add / remove /replace the elements int the begining / middle / end
+
+// arr.splice()
+// arr.splice(1) - it removes the all element after 1st val / 0th index 
+// arr.splice(1,2) - it removes the 1 st and 2nd index values 
+// arr.splice(1,2,("hii",23,[11,12]),"hii") - replaces the elements in the place of removed indexes 
+// arr.splice(0,0,100,200,300) - adds the elements in the begining without removes 
+// arr.splice(-1,0,50,60,70,80) - it adds the elements in the place of last index position / -1 or 3 rd indexes 
+
+
+
+console.log(arr);
+
+
