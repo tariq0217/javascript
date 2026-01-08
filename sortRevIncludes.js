1c7f19b74a0e14db4eac23c2748f6b591e9d0097
tariq0217 <amohammedtariq02@gmail.com>
2026-01-08 09:43:36 +0530

    Ep-39:Learn sorting , reverse , includes

---
+++ sortRevIncludes.js
@@ -0,0 +1,14 @@
+let arr = [5,1,4,6,2,8,111,234,654," ","&",15,23,45,25,34]
+
+arr.sort()  //sort in ASCII order
+
+console.log(arr);
+
+
+let arr1 = [10,20,30,40,50]
+
+arr1.reverse()
+
+console.log(arr1);
+
+console.log(arr1.includes(10));
