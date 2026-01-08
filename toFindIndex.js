f72b8377cc29fdca98fff65f478d7b5c69732878
tariq0217 <amohammedtariq02@gmail.com>
2026-01-08 10:16:36 +0530

    Ep-40:Learn toFind the Index

---
+++ toFindIndex.js
@@ -0,0 +1,10 @@
+let arr = [10,20,30,40,10,50]
+
+let newIndex = arr.indexOf(10)  //left to right / it prints the index of first occured //search start index
+
+console.log(newIndex);
+
+
+let newIndex1 = arr.lastIndexOf(10 , -3)  //right to left / it prints the index of first occured //search start index
+
+console.log(newIndex1);
