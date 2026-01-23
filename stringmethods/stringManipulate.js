fe12852a77f08d24662155d6c14f59fbbafd34a2
tariq0217 <amohammedtariq02@gmail.com>
2026-01-23 19:20:57 +0530

    Ep-45:Learn String Manipulate

---
+++ stringmethods/stringManipulate.js
@@ -0,0 +1,112 @@
+// //charAt
+// let str = "Hello World"
+
+// console.log(str.charAt(str.length-1));
+
+
+// //charCodeAt
+
+// let str1 = "Javascript"
+
+// console.log(str1.charCodeAt(1));
+
+
+// //concat
+// let newStr = str.concat(" ",str1)
+// console.log(newStr);
+
+// //includes
+// let str2 = "Single Threded"
+
+// console.log(str2.includes("i",0));
+
+//indexOf 
+
+// let newString1 = "Kesavn"
+
+// console.log(newString1.indexOf("a",2));
+
+// let newString1 = "Kesavn"
+
+// console.log(newString1.lastIndexOf("a",4));
+
+//Repeat
+
+// let newStr = "Javascript"
+
+// console.log(newStr.repeat(20));
+
+
+//Replace /ReplaceAll
+
+// let str = "Js is a Script lang -Js"
+
+// console.log(str.replaceAll("Js" , "Javascript"));new
+
+//Slice
+
+// let newStr = "Single Thread"
+
+// console.log(newStr.slice(7));
+// console.log(newStr.slice(-7,-2));
+
+
+//Substring & substr
+
+// let newStr1 = "Single Thread"
+
+// console.log(newStr1.substring(3));
+// console.log(newStr1.substring(3,8));
+// console.log(newStr1.substring(8,3));
+
+//Split 
+
+// let words = "My name is iron-Man"
+
+// console.log(words.split(" "));
+// console.log(words.split("-"));
+// console.log(words.split("a",4));
+
+
+//StratsWith
+
+// let words = "My name is ironman"
+
+// console.log(words.startsWith("M"));
+// console.log(words.startsWith("M",5));
+// console.log(words.startsWith("m",5));
+// console.log(words.startsWith("name",));
+
+//EndsWith
+
+// let words = "My name is ironman"
+
+// console.log(words.endsWith("n"));
+// console.log(words.endsWith("n",words.length));
+// console.log(words.endsWith("e",7));
+// console.log(words.endsWith("man",17));
+
+//toLowerCase
+// let words = "My name is ironman"
+
+// console.log(words.toLowerCase());
+
+// toUpperCase
+// let words = "My name is ironman"
+
+// console.log(words.toUpperCase());
+
+//trim 
+
+let words = "   My name is ironman   "
+
+console.log(words.trim());
+
+//trimStart / trimLeft
+console.log(words.trimStart());
+
+//trimEnd / trimRight
+
+console.log(words.trimEnd());
+
+
